11f3c3a08a176e38f2ec13d3ed4a26e5cf18946c
RickyLei <rickylei7@gmail.com>
2020-03-09 21:25:31 -0600

    deploy

---
+++ precache-manifest.fbf847afbf38b80e9167fa926d0a46d0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d95ad8cce9292fd2e8e94e9f16dbba9d",
+    "url": "/rMoney-website/QRCode.jpeg"
+  },
+  {
+    "revision": "e65c89058f3c176fcbe9",
+    "url": "/rMoney-website/css/app.75cc9965.css"
+  },
+  {
+    "revision": "49a43ddb9dad6f4e44dc5625507c0395",
+    "url": "/rMoney-website/index.html"
+  },
+  {
+    "revision": "e65c89058f3c176fcbe9",
+    "url": "/rMoney-website/js/app.476bbb78.js"
+  },
+  {
+    "revision": "2bd43fbc2184e38805a2",
+    "url": "/rMoney-website/js/chunk-vendors.8229a09f.js"
+  },
+  {
+    "revision": "c52fa3a0a801fd44716e83ec391c264f",
+    "url": "/rMoney-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/rMoney-website/robots.txt"
+  }
+]);
